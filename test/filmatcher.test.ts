455bd71f3adfcea4a1271ceaaccaf7c2dc187eab
nathanwick <nathan.wickland@gmail.com>
2025-03-05 16:18:41 -0700

    Initial commit

---
+++ test/filmatcher.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Filmatcher from '../lib/filmatcher-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/filmatcher-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Filmatcher.FilmatcherStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
