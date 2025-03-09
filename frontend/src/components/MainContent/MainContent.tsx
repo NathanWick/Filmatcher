68a486eb2b0d9cd397bd8031ea7766e9417f1786
nathanwick <nathan.wickland@gmail.com>
2025-03-09 13:11:55 -0700

    refactor: Simplify project structure and remove unnecessary files

---
+++ frontend/src/components/MainContent/MainContent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainContent.css';
+
+function MainContent() {
+  return (
+    <div className="main-content">
+      {/* Main content will go here */}
+    </div>
+  );
+}
+
+export default MainContent; 
